2f8d634b717d27866b84828f2280db518d0a5e02
stoler <stoler@ya.ru>
2019-03-09 01:14:07 +0300

    add base for app

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { hydrate, render } from 'react-dom';
+import './index.css';
+import App from './App';
+
+const rootElement = document.getElementById('root');
+
+if (rootElement.hasChildNodes()) {
+    console.log('App: Hydrate');
+    hydrate(<App />, rootElement);
+} else {
+    console.log('App: Render');
+    render(<App />, rootElement);
+}
